2028f7cd03b5705da286f56f6069e981c113e4f0
lalobax42 <lalobax42@gmail.com>
2021-05-01 19:28:56 -0300

    esta es la 3 vez

---
+++ Assets/Scripts/MuvePlayer/MuverPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MuverPlayer : MonoBehaviour
+{
+    
+    [SerializeField] Transform target;
+
+    void Update()
+    {
+        GetComponent<NavMeshAgent>().destination = target.position;
+    }
+}
