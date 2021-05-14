0e0f4798686de5e4c2014082ba045bb236b7befe
estebanjoel <steven.arranzkun@gmail.com>
2021-05-13 23:14:13 -0300

    sonidos

--- Assets/Scripts/Items/ItemPickup.cs
+++ Assets/Scripts/Items/ItemPickup.cs
@@ -6,10 +6,20 @@ namespace RPG.Item
 {
     public abstract class ItemPickup : MonoBehaviour
     {
+        AudioSource audioSource;
+        [SerializeField] AudioClip itemClip;
+
+        void Start()
+        {
+            audioSource = GameObject.Find("PickupSource").GetComponent<AudioSource>();
+        }
+
         private void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.tag == "Player")
             {
+                audioSource.clip = itemClip;
+                audioSource.Play();
                 UseItem(other.gameObject);
                 Destroy(this.gameObject);
             }    
