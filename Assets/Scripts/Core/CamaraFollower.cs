eead4846f423539a376a173bfadb19dfe14d2161
lalobax42 <lalobax42@gmail.com>
2021-06-17 14:56:34 -0300

    camara 1/2

--- Assets/Scripts/Core/CamaraFollower.cs
+++ Assets/Scripts/Core/CamaraFollower.cs
@@ -6,11 +6,39 @@ namespace RPG.Core
 {
     public class CamaraFollower : MonoBehaviour
     {
+        [SerializeField] float camSpeed;
+        [SerializeField] float mauseBorderMuv;
         [SerializeField] private Transform target;
-        // Update is called once per frame
-        void Update()
+        private void Start()
         {
             transform.position = target.position;
+        }
+        void Update()
+        {
+            if (Input.GetKey("space"))
+            {
+                transform.position = target.position;
+            }else
+            {
+                Vector3 pos = transform.position;
+                if (Input.mousePosition.y >= Screen.height - mauseBorderMuv)//w
+                {
+                    pos.z += camSpeed * Time.deltaTime;
+                }
+                if (Input.mousePosition.y <= mauseBorderMuv)//s
+                {
+                    pos.z -= camSpeed * Time.deltaTime;
+                }
+                if (Input.mousePosition.x >= Screen.width - mauseBorderMuv)//d
+                {
+                    pos.x += camSpeed * Time.deltaTime;
+                }
+                if (Input.mousePosition.x <= mauseBorderMuv)//a
+                {
+                    pos.x -= camSpeed * Time.deltaTime;
+                }
+                transform.position = pos;
+            }
         }
     }
 
