652ca193af192cf60c4bb0b0d6b0f88af6be48b2
SantiagoCalogero <75764982+SantiagoCalogero@users.noreply.github.com>
2021-06-22 20:05:16 -0300

    correcciones de escenas(canvas roto),sonidos en carpeta correcta
    se había roto el canvas de las siguientes escenas: win, lose, credit, how to play

---
+++ Assets/santi test/lightVariation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightVariation : MonoBehaviour
+{
+    [SerializeField] Light pointLight;
+    [SerializeField] bool changeIntensity;
+     float intensitySpeed;
+    [SerializeField] float maxIntensity;
+    [SerializeField] bool changeColor;
+    float colorSpeed;
+    [SerializeField] Color startColor;
+    [SerializeField] Color endColor;
+    void Start()
+    {
+        pointLight = GetComponentInChildren<Light>();
+    }
+    private void FixedUpdate()
+    {
+        
+    }
+
+
+    
+}
