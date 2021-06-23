89c50865918ba1de334b3ab0bc07cd0bbd79656e
lalobax42 <lalobax42@gmail.com>
2021-06-23 00:30:09 -0300

    progamacion 1 sol 0

--- Assets/Assets Pack/Fog War/FogOfWar.cs
+++ Assets/Assets Pack/Fog War/FogOfWar.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class FogOfWar : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] GameObject fogOfWarPlane;
+    [SerializeField] Transform playerRef;
+    [SerializeField] LayerMask fogLayer;
+    [SerializeField] float Radio;
+    private float radiosSpr{get{return Radio* Radio; }}
+    private Mesh mesh;
+    private Vector3[] verticesVar;
+    private Color[] colors;
     void Start()
     {
-        
+        Initialize();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Ray r = new Ray(transform.position, playerRef.position - transform.position);
+        RaycastHit hit;
+        if (Physics.Raycast(r, out hit, 1000, fogLayer, QueryTriggerInteraction.Collide))
+        { for (int i = 0; i < verticesVar.Length; i++)
+            {
+                    Vector3 v = fogOfWarPlane.transform.TransformPoint(verticesVar[i]);
+                    float dist=Vector3.SqrMagnitude(v-hit.point);
+                    if (dist < radiosSpr)
+                    {
+                        float alpha = Mathf.Min(colors[i].a, dist / radiosSpr);
+                        colors[i].a = alpha;
+                    }
+            }
         
+        }
+    }
+    void Initialize()//pintar todado los vertises de la niebla de guerra de negro
+    {
+        mesh = fogOfWarPlane.GetComponent<MeshFilter>().mesh;
+        verticesVar = mesh.vertices;
+        colors = new Color[verticesVar.Length];
+        for (int i=0; i< colors.Length; i++)
+        {
+            colors[i] = Color.black;
+        }
+        UpdateColor();
+    }
+    void UpdateColor()
+    {
+        mesh.colors =colors;
     }
 }
