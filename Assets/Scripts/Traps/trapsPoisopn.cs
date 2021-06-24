f370edb3f5c638224e3d9273e5445a1f76bd6e83
SantiagoCalogero <75764982+SantiagoCalogero@users.noreply.github.com>
2021-06-24 20:04:52 -0300

    it A TRAP

---
+++ Assets/Scripts/Traps/trapsPoisopn.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Core;
+using RPG.Combat;
+
+public class trapsPoisopn : poison
+{
+    [SerializeField] float distanceToExlpote;
+    [SerializeField] float distanceToPlayer;
+    [SerializeField] PlayerHealth character;
+    [SerializeField] GameObject vFX;
+    [SerializeField] float currentTime;
+    [SerializeField] float time;
+    
+    void Start()
+    {
+        character = FindObjectOfType<PlayerHealth>();
+        currentTime = time;
+    }
+
+    
+    void Update()
+    {
+        distance();
+        if(distanceToPlayer< distanceToExlpote)
+        {
+            getPoisoned();
+        }
+    }
+
+    void distance()
+    {
+        distanceToPlayer = Vector3.Distance(transform.position, character.transform.position);
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, distanceToExlpote);
+    }
+    public override void getPoisoned()
+    {
+        currentTime -= Time.deltaTime;
+        if(currentTime <= 0)
+        {
+            vFX.SetActive(true);
+            base.getPoisoned();
+            Destroy(this.gameObject, 1.7f);
+        }
+
+        
+    }
+}
