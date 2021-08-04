b62684a4e06ba99171cc073c6c1a5a3e91f11ae8
escuela@davinci.edu.ar <escuela@davinci.edu.ar>
2021-08-04 18:33:38 -0300

    ironMaiden(Glatony para los amigos)
    la dama de heirro giratorio

--- Assets/Scripts/Traps/IronMeiden.cs
+++ Assets/Scripts/Traps/IronMeiden.cs
@@ -1,45 +1,81 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using RPG.Core;
 
 public class IronMeiden : MonoBehaviour
 {
-    [SerializeField] PlayerHealth player3;
+    [SerializeField] NavMeshAgent player3;
     [SerializeField] float goToPlayer;
     [SerializeField] float distanceToPlayer;
+    [SerializeField] float speed;
+    SphereCollider radius;
+    [SerializeField] float targetSpeed;
+    [SerializeField] Transform trapclosing;
+    [SerializeField] Transform closingPivot;
+    [SerializeField] float speedClosing;
+
 
     void Start()
     {
-        player3 = FindObjectOfType<PlayerHealth>();
+        player3 = GameObject.FindWithTag("Player").GetComponent<NavMeshAgent>();
+        radius = GetComponent<SphereCollider>();
+        trapclosing = transform.GetChild(0);
+        closingPivot = GameObject.FindWithTag("pivot").GetComponent<Transform>();
+        radius.radius = goToPlayer;
+        targetSpeed = player3.speed;
     }
-
-    // Update is called once per frame
-    void Update()
+    //cuando el personaje quede atrapado la velocidad la velocidad del player3 debe igualarse a 0 y luego volver a la normalidad con la variable targert
+    private void Update()
     {
-        if (!player3)
-        {
-            distance();
-        }
-
+        distance();
+        
     }
-     
+
+    
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, goToPlayer);
+        Gizmos.color = Color.green;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+            
+        
+        if (distanceToPlayer < goToPlayer)
+        {
+            JumpToFace();
+        }
+    }
+
     void distance()
     {
         distanceToPlayer = Vector3.Distance(transform.position, player3.transform.position);
     }
 
-    private void OnDrawGizmos()
+    void JumpToFace()
     {
-        Gizmos.DrawSphere(transform.position, goToPlayer);
-        Gizmos.color = Color.green;
+        transform.LookAt(player3.transform);
+        float velocity = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, player3.transform.position,velocity);
+        if (distanceToPlayer == 0)
+        {
+            glatony();
+        }
     }
 
-    private void OnTriggerEnter(Collider other)
+    void glatony()
     {
-        if(distanceToPlayer < goToPlayer)
+        float closing = -3;
+        if(closing <= trapclosing.rotation.y)
         {
-          
+            trapclosing.RotateAround(closingPivot.position, Vector3.up, speedClosing * Time.deltaTime);
         }
+        
+        player3.speed = 0;
     }
 }
