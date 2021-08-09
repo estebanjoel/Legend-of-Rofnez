3f08406e51686233d1cb947a5f8b01ffa1deb9b4
escuela@davinci.edu.ar <escuela@davinci.edu.ar>
2021-08-09 19:18:12 -0300

    audio source (inicio)

---
+++ Assets/Scripts/Core/Managers/AudioManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace RPG.Core
+{
+    public class AudioManager : MonoBehaviour
+    {
+        public AudioSource BGM, Ambience;
+        public AudioSource[] PlayerSFXSources;
+        public AudioSource[] EnemySFXSources;
+        public AudioSource ItemSource;
+        public AudioSource UISource;
+
+        public AudioSource ChangeAudioClip(AudioSource mySource, AudioClip myClip)
+        {
+            mySource.clip = myClip;
+            return mySource;
+        }
+
+        public void PlayBGM()
+        {
+            BGM.Play();
+        }
+
+        public void StopBGM()
+        {
+            BGM.Stop();
+        }
+
+        public void PlayAmbience()
+        {
+            Ambience.Play();
+        }
+
+        public void StopAmbience()
+        {
+            Ambience.Stop();
+        }
+    }
+
+}
