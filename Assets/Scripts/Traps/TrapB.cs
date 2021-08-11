b35d123541a08b2acc1cfa9ee6a5b175751463a2
estebanjoel <steven.arranzkun@gmail.com>
2021-08-11 00:00:35 -0300

    trampas programadas

--- Assets/Scripts/Traps/TrapB.cs
+++ Assets/Scripts/Traps/TrapB.cs
@@ -6,19 +6,15 @@ using RPG.Core;
 public class TrapB : MonoBehaviour
 {
     [SerializeField] private int damage;
-    [SerializeField] private Vector3 impulse;
+    //[SerializeField] private Vector3 impulse;
     protected void OnTriggerEnter(Collider other)
     {
-        if (other.GetComponent<PlayerHealth>() != null)
-        {
-            
-            Rigidbody playerForse = other.GetComponent<Rigidbody>();
-            playerForse.AddForce(impulse, ForceMode.Impulse);
+        if (other.tag == "Player")
+        {            
+            // Rigidbody playerForse = other.GetComponent<Rigidbody>();
+            // playerForse.AddForce(impulse, ForceMode.Impulse);
             Health player = other.GetComponent<Health>();
-            player.TakeDamage(20);
-            Debug.Log(impulse);
-
-
+            player.TakeDamage(damage);
         }
     }
 }
